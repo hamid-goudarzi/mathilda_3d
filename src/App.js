81d0e143796e3b4b90022a3b94929ee5f667bc74
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2024-03-16 04:38:29 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,61 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
-import {Canvas, useFrame, extend, useThree} from 'react-three-fiber'
-import * as THREE from 'three'
-import './App.css'
+import React, { useEffect, useRef, useState } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
+import * as THREE from "three";
+import "./App.css";
 
-  extend({OrbitControls})
-  
-const MyModel =()=>{
+extend({ OrbitControls });
 
-    const [model, setModel] = useState()
+const MyModel = () => {
+  const [model, setModel] = useState();
 
-    useEffect(()=>{
-      new GLTFLoader().load("/model/scene.gltf", setModel)
-    })
-   
-      return model ? <primitive object ={model.scene} />: null;
-}
+  useEffect(() => {
+    new GLTFLoader().load("/model/scene.gltf", setModel);
+  });
 
+  return model ? <primitive object={model.scene} /> : null;
+};
 
-  const Controls=()=>{
-    const orbitRef = useRef()
-    const {camera, gl} = useThree();
+const Controls = () => {
+  const orbitRef = useRef();
+  const { camera, gl } = useThree();
 
-    useFrame(()=>{
-      orbitRef.current.update()
-
-    })
-    return(
-      <orbitControls
+  useFrame(() => {
+    orbitRef.current.update();
+  });
+  return (
+    <orbitControls
       args={[camera, gl.domElement]}
-      ref ={orbitRef}
+      ref={orbitRef}
       autoRotate
-      maxPolarAngle={Math.PI/2}
-      minPolarAngle={Math.PI/3}
-      />
-    )
-  }
-
-
+      maxPolarAngle={Math.PI / 2}
+      minPolarAngle={Math.PI / 3}
+    />
+  );
+};
 
 const App = () => {
+  return (
+    <>
+      <h1 class="w3-row">
+        Mathi<span>l</span>da
+      </h1>
 
+      <Canvas
+        camera={{ position: [200, 200, 200] }}
+        onCreated={({ gl }) => {
+          gl.shadowMap.enabled = true;
+          gl.shadowMap.type = THREE.PCFSoftShadowMap;
+        }}
+      >
+        <ambientLight intencity={0.5} />
+        <spotLight position={[15, 20, 5]} penumbra={1} castShadow />
+        <Controls />
+        <MyModel />
+      </Canvas>
+    </>
+  );
+};
 
-  return(
-<>
-  <h1 class="w3-row">Mathi<span>l</span>da</h1>
-
-  <Canvas camera={{position:[200,200,200]}} onCreated={({ gl })=>{
-    gl.shadowMap.enabled = true 
-    gl.shadowMap.type = THREE.PCFSoftShadowMap
-
-  }}>
-    <ambientLight intencity ={0.5}/>
-    <spotLight position={[15,20,5]} penumbra={1} castShadow/> 
-    <Controls/>
-    <MyModel/>
-  </Canvas>
-  </>
-  ) 
-}
-
-export default App
-
-  
+export default App;
